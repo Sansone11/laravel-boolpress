33a9ea0bfaabd0d3cafaf36c6ebc978ef4ea9256
Sansone11 <sansonemirco@gmail.com>
2022-10-27 19:18:52 +0200

    fix-bug

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{title}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title:'Bentornato js!!ora faremo i conti'
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
